b605efdac8a03154ea507d7b191a24f8ab5043ab
Chris Jones <chris@pythonistic.com>
2012-11-23 08:09:48 -0700

    Added facet list, added facet search tester.
    Added facet list, added facet search tester.  Bug in index.py on facets
    (empty list -- either unprocessed or skipped).

--- index.py
+++ index.py
@@ -25,8 +25,6 @@ def hello(name):
  
 @route('/search/:keyword')
 def search(keyword):
-    host = config.get("zappos", "API-host")
-    key = config.get("zappos", "API-key")
     url = host + "/Search/term/%s?key=%s" % (quote_plus(keyword), key)
     try:
         response = urllib2.urlopen(url)
@@ -36,9 +34,84 @@ def search(keyword):
         return "<pre>" + str(out) + "</pre>"
     except URLError as e:
         return "<font color=\"red\">Error: %s</font>" % (str(e))
+
+@route('/searchsize/:keyword', method="POST")
+def searchsize(keyword):
+    sex = request.forms.sex
+    request_facets = {}
+    if sex == 'M':
+        for facet in mens_facets:
+            if facet in request.forms:
+                request_facets[facet] = request.forms[facet]
+    elif sex == 'F':
+        for facet in womens_facets:
+            if facet in request.forms:
+                request_facets[facet] = request.forms[facet]
+    for facet in universal_facets:
+        if facet in request.forms:
+                request_facets[facet] = request.forms[facet]
+    
+    url = host + "/Search/term/%s?filters=%s&key=%s" % (quote_plus(keyword), build_facets(request_facets), key)
+    try:
+        response = urllib2.urlopen(url)
+        out = ""
+        for line in response:
+            out += line
+        return "<b>REQUEST</b><br/><pre>" + str(url) + "</pre><p/><b>RESPONSE</b><br/><pre>" + str(out) + "</pre>"
+    except URLError as e:
+        return "<font color=\"red\">Error: %s</font>" % (str(e))
+
+def build_facets(request_facets):
+    out = {}
+    for facet in request_facets:
+        if type(request_facets[facet] == str):
+            out['"' + facet + '"'] = '["' + request_facets[facet] + '"]'
+        elif type(request_facets[facet] == list):
+            values = []
+            for value in request_facets[facet]:
+                values.append('"' + value + '"')
+            out['"' + facet + '"'] = '[' + ",".join(values) + ']'
+    
+    return out
+    
+mens_facets = [    
+"hc_men_apparel_blazer_size",
+"hc_men_apparel_bottom_belt_size",
+"hc_men_apparel_bottom_big_tall_size",
+"hc_men_apparel_bottom_size",
+"hc_men_apparel_bottom_waist_size",
+"hc_men_apparel_bottom_width",
+"hc_men_apparel_button_up_shirt_size",
+"hc_men_apparel_general_size",
+"hc_men_apparel_inseam",
+"hc_men_apparel_top_big_tall_size",
+"hc_men_footwear_socks_size",
+"hc_men_size",
+"hc_men_width"
+]
+
+womens_facets = [
+"hc_women_apparel_bottom_belt_size",
+"hc_women_apparel_bottom_size",
+"hc_women_apparel_bottom_waist_size",
+"hc_women_apparel_bottom_width",
+"hc_women_apparel_general_size",
+"hc_women_apparel_inseam",
+"hc_women_apparel_top_size",
+"hc_women_footwear_socks_size",
+"hc_women_size",
+"hc_women_width"
+]
+
+universal_facets = [
+    "size",
+    "colorFacet"
+]
     
 config = ConfigParser.SafeConfigParser()
 config.read("gangnam.config")
+host = config.get("zappos", "API-host")
+key = config.get("zappos", "API-key")
 
 if __name__ == '__main__':
     CGIHandler().run(bottle.default_app())
